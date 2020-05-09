f362e87e10596e562830887faa2049bd0b964e48
Jared Dobro <jareddobro@Jareds-MacBook-Pro.local>
2020-05-09 16:42:16 -0400

    sticker overlay js

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,68 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "292.5px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "292.5px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+$(document).ready(function () {
+  $("#henry").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/henry.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#icarly").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/icarly.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#john1").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/john_cena.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#john2").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/john_cena.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#jojo1").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/jojo.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#jojo2").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/jojo.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#lincoln").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/lincoln.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#spongebob").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/spongebob.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#tori").click(function () {
+    console.log("clicked");
+    $("#overlayimg").attr("src", "../img/stickers/tori.png");
+    $("#overlay").fadeIn();
+  });
+
+  $("#overlay").click(function () {
+    $("#overlay").fadeOut();
+  })
+});
