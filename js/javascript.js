9f8e14643e232b1de29080f186a9e0d2700832f9
Jared Dobro <jareddobro@Jareds-MacBook-Pro.local>
2020-05-09 19:03:37 -0400

    set up profile page

--- js/javascript.js
+++ js/javascript.js
@@ -6,8 +6,63 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+function changeLetter(letterinput) {
+  $("#title").html(letterinput.title);
+  $("#lttr").html(letterinput.lttr)
+
+}
+
 
 $(document).ready(function () {
+
+
+  //setting up letter here
+
+  class letter {
+    constructor(title, lttr) {
+      this.title = title;
+      this.lttr = lttr;
+    }
+  }
+
+  //setting up array
+
+  let letters = []
+
+  let lttr1 = new letter("Letter 1", "Inster letter txt here");
+  let lttr2 = new letter("Letter 2", "Inster letter txt here");
+  let lttr3 = new letter("Letter 3", "Inster letter txt here");
+  let lttr4 = new letter("Letter 4", "Inster letter txt here");
+
+  letters.push(lttr1);
+  letters.push(lttr2);
+  letters.push(lttr3);
+  letters.push(lttr4);
+
+
+  //initial screen
+  changeLetter(letters[0]);
+
+  //button setup
+
+  let whichletter = 0;
+
+  $("#lttrbutton").click(function () {
+    whichletter = whichletter + 1;
+    console.log("clicked");
+    console.log(whichletter);
+
+    if (whichletter >= letters.length) {
+      whichletter = 0;
+
+    }
+
+    changeLetter(letters[whichletter])
+  });
+
+
+
+  //sticker page js
   $("#henry").click(function () {
     console.log("clicked");
     $("#overlayimg").attr("src", "../img/stickers/henry.png");
